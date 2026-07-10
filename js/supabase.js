ba33aa37a6de03d6e63d978ac8f4cc203ad4254e
minimodatrabalho-bit <minimodatrabalho@gmail.com>
2026-07-09 21:30:30 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,12 @@
-teste
+// Configuração do Supabase
+
+const SUPABASE_URL = "https://ejmnpegmicovtuihyknv.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_9JxyEQs6m_hMmqFK1Vo35g_sCy6GJbe";
+
+// Inicializa o cliente
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
